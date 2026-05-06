85fdddda196b44ab23e30372103259fafcf67e68
Reza Kamali <mrgando@protonmail.com>
2026-05-06 10:54:55 +0330

    minor responsive / routing issues got fixed

--- component/UI/Buttons.tsx
+++ component/UI/Buttons.tsx
@@ -11,7 +11,7 @@ export function RedirectButton({ title, url, theme, style }: Props) {
   if (theme === "GreenToBlue") {
     return (
       <a
-        className={`bg-[#96E92A] rounded-lg px-7 py-3 text-[#0B1221] font-medium text-[1rem] hover:bg-[#0099ff]/90 hover:text-white transition-colors duration-300`}
+        className={`bg-[#96E92A] rounded-lg px-7 py-3 text-[#0B1221] font-medium text-sm sm:text-[1rem] hover:bg-[#0099ff]/90 hover:text-white transition-colors duration-300`}
         role="button"
         href={url}>
         {title}
@@ -20,7 +20,7 @@ export function RedirectButton({ title, url, theme, style }: Props) {
   }
   return (
     <a
-      className={`bg-[#0099ff] rounded-lg px-7 py-3 text-white font-medium text-[1rem] hover:text-[#0B1221] hover:bg-[#96E92A]/90 transition-colors duration-300 `}
+      className={`bg-[#0099ff] rounded-lg px-7 py-3 text-white font-medium text-sm sm:text-[1rem] hover:text-[#0B1221] hover:bg-[#96E92A]/90 transition-colors duration-300 `}
       role="button"
       href={url}>
       {title}
